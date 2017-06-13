6f6622696cf5d7ddbf9855af5bbc3ace9c4ae35f
omyahro <omyah.ro@gmail.com>
2017-06-13 15:38:46 -0400

    Add files via upload

---
+++ shapes(trianlge).py
@@ -0,0 +1,19 @@
+from turtle import *
+import math
+
+# Name your Turtle.
+t = Turtle()
+t.speed(1)
+
+# Set Up your screen and starting position.
+setup(900, 600)
+x_pos = -250
+y_pos = -150
+t.setposition(x_pos, y_pos)
+
+### Write your code below:
+
+t.left(90)
+t.forward(350)
+t.right(129)
+t.forward(350)
---
+++ shapes.py
@@ -0,0 +1,21 @@
+from turtle import *
+import math
+
+# Name your Turtle.
+t = Turtle()
+t.speed(1)
+
+# Set Up your screen and starting position.
+setup(900, 600)
+x_pos = -250
+y_pos = -150
+t.setposition(x_pos, y_pos)
+
+### Write your code below:
+
+t.left(120)
+t.forward(250)
+t.right(90)
+t.forward(250)
+t.right(90)
+t.forward(250)
